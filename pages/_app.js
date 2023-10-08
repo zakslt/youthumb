5ac20d30ca4ec1f22fee4ed0d3d27b98b8065818
zakslt <134002302+zakslt@users.noreply.github.com>
2023-10-08 22:15:28 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,9 @@ function MyApp({ Component, pageProps }) {
       <DefaultSeo
         title="Youtube Thumbnail Downloader"
         description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://your-website-url.com"
+        canonical="https://ytbthumbnail.com"
         openGraph={{
-          url: "https://your-website-url.com",
+          url: "https://ytbthumbnail.com",
           title: "Youtube Thumbnail Downloader",
           description: "Download high-quality thumbnails from YouTube videos.",
           site_name: "Youtube Thumbnail Downloader",
