23052ebe2c2e44f0884fa2ac085bed9b234ed400
zakslt <134002302+zakslt@users.noreply.github.com>
2023-10-08 22:53:55 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <meta name="description" content="4K full HD youtube thumbnail downloader extractor. Unlock the Power of YouTube Thumbnails with Our Thumbnail Extractor Tool! 📸 Want to enhance your YouTube video marketing strategy? Our Thumbnail Extractor lets you effortlessly retrieve high-quality thumbnails from any YouTube video. Create eye-catching, click-worthy thumbnails for your own videos or gain inspiration from top creators. Boost your video's visibility and engagement with stunning visuals. Try our YouTube Thumbnail Extractor today and take your channel to the next level!">
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9262230510365108"
+       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9262230510365108"
+     crossorigin="anonymous"></script>
           <link
             rel="apple-touch-icon"
             sizes="180x180"
