c3af53a136682d2d43ee51e0547de0b6c744d29b
zakslt <134002302+zakslt@users.noreply.github.com>
2023-10-11 23:52:36 +0200

    HHHH

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,7 @@ const Index = () => {
   };
 
   return (
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <div className="container mx-auto px-4 py-8">
       <header className="text-center mb-8">
         <h1 className="text-3xl font-bold mb-2">
