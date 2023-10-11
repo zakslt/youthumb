96171c6fa2ecc1ce343f06c0c4adc2ba598cad11
zakslt <134002302+zakslt@users.noreply.github.com>
2023-10-11 23:56:10 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-      <link rel="stylesheet" type="text/css" href="styles.css">
+      <link rel="stylesheet" type="text/css" href="styles.css"/>
        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9262230510365108"
      crossorigin="anonymous"></script>
           <link
